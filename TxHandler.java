92d14c605967a290a9e100e4e59936db22e84f17
Nelson Korneev Terra <na.terra@gmail.com>
2017-06-12 00:36:47 -0300

    Finished method isValidTx; Info to class diagram

--- TxHandler.java
+++ TxHandler.java
@@ -24,22 +24,64 @@ public class TxHandler {
     public boolean isValidTx(Transaction tx) {
         // IMPLEMENT THIS
 
-        // (1) for o in all tx.i if utxo.contains(o)
-        for (Transaction.Input txIn : tx.getInputs) {
-            UTXO probeUtxo(txIn.prevTxHash, txIn.outputIndex)
+        ArrayList<UTXO> claimedUtxos;
 
-            if (!utxoPool.contains(probeUtxo))
+        float sumOfInputs = 0; // sum of outputs claimed by inputs
+        float sumOfOutputs = 0;
+
+        // (1) all outputs claimed by {@code tx} are in the current UTXO pool,
+        // for o in all tx.i if utxo.contains(o)
+        // Outputs claimed by transactions are outputs in the UTXO referenced by transactions inputs
+
+        // for all transaction inputs
+        for (Transaction.Input txIn : tx.getInputs()) {
+
+            // Construct a claimedUtxo to test "contains" in the UTXOPool
+            UTXO claimedUtxo(txIn.prevTxHash, txIn.outputIndex)
+
+            // If not every utxo referenced by an input in the transaction is in the UTXOPool, "isValidTx" fails
+            if (!utxoPool.contains(claimedUtxo))
                 return false;
 
-            else {
-              // (2) for all i in tx.i if Crypto.verifySignature(o.pk, tx.getRawTx, i.sig)
+            // (2) the signatures on each input of {@code tx} are valid,
+            // for all i in tx.i if Crypto.verifySignature(o.pk, tx.getRawTx, i.sig)
 
-              Transaction.Output sigOut = utxoPool.getTxOutput(probeUtxo);
-              if (Crypto.verifySignature(sigOut.address, txIn.getRawTx(), tx.signature));
-            }
+            // Get the transaction that generated the claimed utxo
+            Transaction.Output claimedOutput = utxoPool.getTxOutput(claimedUtxo);
+
+            // Verify signature
+            if (!Crypto.verifySignature(claimedOutput.address, txIn.(txIn.getInputs.indexOf(txIn)), tx.signature))
+                return false;
+
+            // (3) no UTXO is claimed multiple times by {@code tx}
+            if(claimedUtxos.contains(claimedUtxo))
+              return false;
+
+            // add claimedOutput value to the amount of input values to this transaction
+            sumOfInputs += claimedOutput.value;
+
+            // add claimedUtxo to the claimed utxo list for the next inputs to
+            // verify diouble spending within this transaction
+            claimedUtxos.add(claimedUtxo);
         }
 
 
+        // (4) all of {@code tx}s output values are non-negative
+        for (Transaction.Output txOut : tx.getOutputs()) {
+
+          if(txOut.value < 0) {
+            return false;
+
+          sumOfOutputs += txOut.value;
+
+        }
+
+        // (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+        // values; and false otherwise.
+        if(!(sumOfInputs >= sumOfOutputs))
+          return false;
+
+        return true;
 
     }
 
@@ -50,6 +92,10 @@ public class TxHandler {
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
         // IMPLEMENT THIS
+
+        // validate each transaction individually
+
+        // create a temporary utxo pool
     }
 
 }
